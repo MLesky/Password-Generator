bdf28b8e605137e3233705bc4bf99b0d8f0e8430
MLesky <mbahlesky2@gmail.com>
2022-10-05 09:54:25 +0100

    refactor: rearrange layout

--- kivy version/main.py
+++ kivy version/main.py
@@ -5,8 +5,10 @@ from kivymd.uix.boxlayout import MDBoxLayout
 class Options(MDBoxLayout):
     pass
 
-class PasswordApp(MDApp):
-    pass 
 
-if __name__=="__main__":
+class PasswordApp(MDApp):
+    pass
+
+
+if __name__ == "__main__":
     PasswordApp().run()
